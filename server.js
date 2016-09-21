bbf63cd6a3eb669fac775d185a6581bbe0ae60f9
saikrishnakotnala <saikrishnachinnu8@gmail.com>
2016-09-22 00:42:02 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ app.get('/ui/5stars.png', function (req, res) {
 app.get('/ui/borderleft.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '/ui/borderleft.png'));
 });
+app.get('/ui/imagelight.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/imagelight.png'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
