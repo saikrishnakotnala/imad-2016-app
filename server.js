e4577736fac7fbf80ea20f0bf596299e1e75db0f
saikrishnakotnala <saikrishnachinnu8@gmail.com>
2016-09-22 00:21:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,12 +12,33 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/bg.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bg.png'));
 });
-
-
+app.get('/ui/1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
+});
+app.get('/ui/0stars.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/0stars.png'));
+});
+app.get('/ui/1stars.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/1stars.png'));
+});
+app.get('/ui/2stars.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/2stars.png'));
+});
+app.get('/ui/3stars.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/3stars.png'));
+});
+app.get('/ui/4stars.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/4stars.png'));
+});
+app.get('/ui/5stars.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/5stars.png'));
+});
+app.get('/ui/borderleft.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/ui/borderleft.png'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
